8ffa8312b61de591e32f4e6a59fff0065113ff29
16Bogdan54 <b.yunakov1654@gmail.com>
2023-05-23 14:02:07 +0300

    pagination. comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @categories = Category.all
-    @posts = Post.paginate(page: params[:page], per_page: 5)
+    @posts = Post.paginate(page: params[:page], per_page: 9)
   end
 
   # GET /posts/1 or /posts/1.json
